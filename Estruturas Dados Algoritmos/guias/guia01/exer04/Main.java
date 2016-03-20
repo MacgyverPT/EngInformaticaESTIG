20fe93c224686ff3b8bdf1b873734e37e2fe3fdc
mike <miguel.nuno.rosa@gmail.com>
2016-03-20 11:02:20 +0000

    EDA Guia 1: Exercicio InsertionSort

---
+++ Estruturas Dados Algoritmos/guias/guia01/exer04/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int A[] = {5, 2, 4, 6, 1, 3, 15, 10, 22, 14, 7};
+		
+		System.out.printf("\nArray Original: ");
+        for(int x: A){
+            System.out.print(" " + x + ", ");
+        }
+        
+        //Bubble Sort
+        InsertionSort is = new InsertionSort();
+        
+        long tempoInicial = System.currentTimeMillis();
+        is.sort(A);
+        long tempoFinal = System.currentTimeMillis();
+        
+        
+        
+        System.out.printf("\nArray Ordenado: ");
+        for(int x: A){
+            System.out.print(" " + x + ", ");
+        }
+        
+        //saber o tempo em segundos (dividir por 1000)
+        System.out.printf("\n\nTempo utilizado: %.3f\n", (tempoFinal - tempoInicial) / 1000d);
+    }
+	
+}
