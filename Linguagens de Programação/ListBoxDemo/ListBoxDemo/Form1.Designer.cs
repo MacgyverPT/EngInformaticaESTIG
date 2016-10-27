51cfcca951170542fef68d3d78beebc59eb14d8b
Miguel <mike@MacBook-Pro-de-Miguel.local>
2016-10-27 15:52:59 +0100

    add LP: ListboxDemo

---
+++ Linguagens de Programação/ListBoxDemo/ListBoxDemo/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ListBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Apple",
+            "Apricot",
+            "Avocado",
+            "Banana",
+            "Bilberry",
+            "Blackberry",
+            "Blackcurrant",
+            "Blueberry",
+            "Boysenberry",
+            "Currant",
+            "Cherry",
+            "Cherimoya",
+            "Cloudberry",
+            "Coconut",
+            "Cranberry"});
+            this.listBox1.Location = new System.Drawing.Point(27, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 147);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(27, 182);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(111, 181);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 373);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Linguagens de Programação/ListBoxDemo/ListBoxDemo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int pos = listBox1.SelectedIndex;
+            String fruit = listBox1.SelectedItem.ToString();
+            //MessageBox.Show(String.Format("Fruit name: {0} on position {1}", fruit, pos));
+        }
+
+        private String askSaveFilename()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt | All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Title = "Save File...";
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return saveFileDialog1.FileName;
+            }
+
+            return null;
+        }
+
+        private void saveListToFile(String filename, List<String> list)
+        {
+            System.IO.File.WriteAllLines(filename, list);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String filename = this.askSaveFilename();
+
+            if (filename != null)
+            {
+                MessageBox.Show(filename);
+                List<String> list = new List<string>();
+
+                foreach (String item in this.listBox1.Items)
+                {
+                    list.Add(item);
+                }
+
+                this.saveListToFile(filename, list);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            String filename = @"C:\Users\MV\Documents\as.txt";
+            String[] fruits = System.IO.File.ReadAllLines(filename);
+            this.listBox1.Items.Clear();
+            this.listBox1.Items.AddRange(fruits);
+        }
+    }
+}
